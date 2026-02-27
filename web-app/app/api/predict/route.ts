566c32cf296237756e37c6348bcc4c22328a5457
Danizhj <danikhihanik@gmail.com>
2026-02-27 07:21:27 +0500

    changed post route to deploy

--- web-app/app/api/predict/route.ts
+++ web-app/app/api/predict/route.ts
@@ -3,11 +3,14 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  const res = await fetch("http://backend:8000/predict", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
-  });
+  const res = await fetch(
+    "https://ufc-fights-prediction-backend.onrender.com/predict",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    },
+  );
 
   const data = await res.json();
   return NextResponse.json(data);
